4d9f5a106f937104441e053d56e45f5af02e7f05
hgb22613 <hgb22613@SJ-010014.wsdc.com>
2017-11-26 15:33:38 +0800

    Chat 0.2 添加Closing事件

--- src/guyue/hu/ChatClient.java
+++ src/guyue/hu/ChatClient.java
@@ -3,6 +3,8 @@ package guyue.hu;
 import java.awt.BorderLayout;
 import java.awt.Frame;
 import java.awt.TextArea;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class ChatClient {
 
@@ -14,17 +16,31 @@ public class ChatClient {
 }
 
 class MyFrame extends Frame {
+
+	TextArea chatArea = null;
+	TextArea inputArea = null;
+
 	public MyFrame() {
 		super("Chat 1.0");
 	}
-	
+
 	public void launch() {
-		TextArea chatArea = new TextArea("", 20, 50, TextArea.SCROLLBARS_VERTICAL_ONLY);
-		TextArea inputArea = new TextArea("", 5, 50, TextArea.SCROLLBARS_VERTICAL_ONLY);
+		chatArea = new TextArea("", 20, 50, TextArea.SCROLLBARS_VERTICAL_ONLY);
+		inputArea = new TextArea("", 5, 50, TextArea.SCROLLBARS_VERTICAL_ONLY);
 		chatArea.setEditable(false);
 		add(chatArea, BorderLayout.NORTH);
 		add(inputArea, BorderLayout.SOUTH);
+		addWindowListener(new Closing());
 		pack();
 		setVisible(true);
+	}
+
+	class Closing extends WindowAdapter {
+
+		@Override
+		public void windowClosing(WindowEvent arg0) {
+			System.exit(0);
+		}
+
 	}
 }
