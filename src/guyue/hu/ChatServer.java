079e21eeb2374a8d0e05138960157581dc8c89d5
guyuehu <398456458@qq.com>
2017-11-27 20:05:25 +0800

    Chat 0.8 Server端异常处理

--- src/guyue/hu/ChatServer.java
+++ src/guyue/hu/ChatServer.java
@@ -8,13 +8,19 @@ import java.net.Socket;
 public class ChatServer {
 	public static void main(String[] args) {
 		boolean connect = false;//客户端是否连接上服务器端
+		ServerSocket serverSocket = null;
+		String str = null;
+		Socket socket = null;
+		DataInputStream dis = null;
 		
 		try {
-			ServerSocket serverSocket = new ServerSocket(18888);
+			serverSocket = new ServerSocket(18888);
+		} catch	(IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
 			connect = true;
-			Socket socket = null;
-			String str = null;
-			DataInputStream dis = null;
 			while(connect) {
 				boolean accepted = false;//服务器端是否接收
 				socket = serverSocket.accept();
@@ -25,10 +31,20 @@ System.out.println("A client connect!");
 					str = dis.readUTF();
 					System.out.println(str);
 				}
-				dis.close();
 			}
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("Client closed!");
+		} finally {
+			try {
+				if(dis != null) {
+					dis.close();
+				}
+				if(socket != null) {
+					socket.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
